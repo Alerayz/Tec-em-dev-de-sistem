40cbebfad4abe2a8cb4926013e4eae01a7a40c66
Alexandre M <alexandremoraesdemendonca@gmail.com>
2024-11-12 09:07:45 -0300

    Programas em Python
    Adicionando Programas em Python no Github

---
+++ Antecessor e Sucessor.py
@@ -0,0 +1,10 @@
+#Solicita ao Usuário para digitar m número
+valor = int(input("Digite um número: "))
+
+#Calcula o antecessor e o sucessor
+antecessor = valor - 1
+sucessor = valor + 1
+
+#Exibe o resiltado do antecessor e sucessor
+print(f"O antecessor do {valor} é {antecessor}.")
+print(f"O sucessor de {valor} é {sucessor}.")
