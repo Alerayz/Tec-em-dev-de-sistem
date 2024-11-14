369f8088865bc3cb8b50cf7787707a6a68d859f8
Alerayz <alexandremoraesdemendonca@gmail.com>
2024-11-14 11:32:31 -0300

    Atualização
    Correção Aula 03 Repetições

---
+++ Python/Aula 03/Correção Atvd 02 Aula 03.py
@@ -0,0 +1,14 @@
+# num_tabuada = 8
+
+# i = 0
+# while i <= num_tabuada:
+#     print(f'{i} x {num_tabuada} = {i * num_tabuada}')
+#     i += 1
+
+num_tabuada = int(input('Qual tabuada deseja:'))
+
+i = 0
+while i <= num_tabuada:
+    print(f'{i} x {num_tabuada} = {i * num_tabuada}')
+    i += 1
+    
