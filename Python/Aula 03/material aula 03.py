bb851a251cd71f139eb1edbd5904e3c5740f1200
Alerayz <alexandremoraesdemendonca@gmail.com>
2024-11-14 09:05:00 -0300

    Material Aula 03
    Material Condição

---
+++ Python/Aula 03/material aula 03.py
@@ -0,0 +1,25 @@
+#enquanto condição for true. farei denovo
+#break - quebra de repetição
+
+condicao = True
+
+# while condicao:
+#     input('Olá, Tudo bem?')
+
+#     if pergunta == 'nao':
+#         break
+
+
+contador = 0
+    
+while contador <= 10:
+        contador += 1
+        
+        if contador == 5:
+            print('Não vou mostrar o número 5 ')
+            continue
+
+        print(contador)
+        # contador = contador + 1
+        
+
