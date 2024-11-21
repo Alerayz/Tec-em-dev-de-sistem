e8e8f3f1b96562b79b629f3904d2c600eb82b9d0
Alerayz <alexandremoraesdemendonca@gmail.com>
2024-11-21 13:45:35 -0300

    Update Aula 05

--- Python/Aula 05 - Funções/Atividade 03.py
+++ Python/Aula 05 - Funções/Atividade 03.py
@@ -2,7 +2,7 @@ from os import system as sy
 import time
 sy('cls')
 
-def calcular_media_ponderada(nota1, nota2, nota3):
+def calcular_media_ponderada(nota1, nota2, nota3)
     peso1, peso2, peso3 = 2, 3, 5
     
     media = round((nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3), 2)
@@ -12,6 +12,7 @@ nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 
+media = calcular_media_ponderada(nota1, nota2, nota3)
 media_final = calcular_media_ponderada(nota1, nota2, nota3)
 
 media_final_arredondada = round(media_final, 2)
@@ -20,10 +21,3 @@ response = media_final_arredondada
 print(f'A média do aluno é: {response:.2f}' , end=' ')
 print(f'A média final do aluno é: {round(media_final_arredondada, 2)}')
 print('\033[32mAPROVADO!\033[m' if response >= 7 else '\033[31mREPROVADO!\033[m')
-
-def somarMuitos(*medias):
-    valortotal = 0    
-
-    for numero in medias:
-        valortotal += numero
-    print(medias, valortotal)
